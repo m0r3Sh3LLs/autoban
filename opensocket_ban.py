cee208865da601ef0b2ec51bd9974ea75a278c38
m0r3Sh3LLs <chris.a.kuehl@gmail.com>
2013-04-30 01:42:39 -0300

    Update opensocket_ban.py

--- opensocket_ban.py
+++ opensocket_ban.py
@@ -21,6 +21,12 @@ import sys
 import os
 import random
 
+#Store all the banned ips here so that you dont keep banning the same ip
+bannedIps = []
+
+
+#Define the socket listener function.
+#Except two variables, the host ip string, and a port as an integer
 def startListen(host, port):
 
 	# Create a TCP/IP socket
@@ -31,35 +37,67 @@ def startListen(host, port):
 	sock.bind(server_address)
 	print >>sys.stderr, 'starting up on %s port %s' % sock.getsockname()
 	sock.listen(1)
-	
-	# Loop forever
+
+	#Create an infinite loop in order to wait for a connection
 	while True:
 		print >>sys.stderr, 'waiting for a connection'
 		connection, client_address = sock.accept()
 		try:
 			print >>sys.stderr, 'client connected:', client_address
-			while True:
-				addr = str(client_address).replace('(','')
-				addr = addr.replace('\'','')
-				split = addr.split(',')
-				ip = split[0]
-				
-				
-				autoBan(ip)
-				break
+			
+
+			#Clean up the client_address variable, as it has a port number on the end and some other special characters
+			addr = str(client_address).replace('(','')
+			addr = addr.replace('\'','')
+			split = addr.split(',')
+			
+			#after the clean up you should end up with just an ip address
+			ipToBan = split[0]
+			print "I am banning: "+ipToBan
+			
+			#check if the ip you are trying to banned has already been autobanned, sometimes the connection wont close in time or you forgot to turn your firewall on
+			isIpBanned = False
+			for bannedIp in bannedIps:
+				if bannedIp == ipToBan:
+					isIpBanned = True
+					print "Check your firewall, ip has been banned already... :"+ipToBan
+					break
+					
+			if isIpBanned == False:
+				autoBan(ipToBan)
+					
+			#Check if the ip address matchs your banned ip list
+			#send the clients ip that connected to you over to autoBan function
+				autoBan(ipToBan)
+
 		finally:
 			connection.close()
-			
-# Autoban function # Creates a randomized name for the firewall rule
+
+# Define autoBan function and accept an ip address
+# This function will generate a random number, and a command
+# Its using the netsh firewall command to add a rule to block the ip address you passed to it.
+# The random number is generated to add to the rule name so it doesnt over write
 def autoBan(host):
 	randomNum=str(random.randrange(1, 1000000))
 	command="netsh advfirewall firewall add rule name=\"BAN"+randomNum+"\" protocol=TCP action=block dir=IN remoteip="+host
 	print command
 	os.system(command)
 	
-### MAIN APPLICATION ####
+	#add the ip to the list of banned ips
+	bannedIps.append(host)
 
-#Listen on any interface
-host='0.0.0.0'
-port=22
-startListen(host, port)
+	
+### MAIN APPLICATION ####
+if __name__ == '__main__':
+	
+		#Print banner
+	print "\n\n\n\n-----------------------------------"
+	print "Auto Ban Hosts on Scan v1.0 by m0r3Sh3LLs"
+	print "-----------------------------------\n\n\n\n"
+	
+	
+	#Listen on any address using 0.0.0.0
+	host='0.0.0.0'
+	port=22
+	#start your listener, and pass the host string, and port number
+	startListen(host, port)
